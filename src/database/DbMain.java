2b5940be915b37bf3d2354e7cd837573c07b4584
LinFeng <china2012syl@163.com>
2016-09-13 01:20:57 +0800

    登录注册 初步准备

--- src/database/DbMain.java
+++ src/database/DbMain.java
@@ -2,28 +2,24 @@ package database;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
 
 public class DbMain {
-	private String driver = "com.mysql.jdbc.Driver"; // 驱动
-	private String url = "jdbc:mysql://localhost:3306/library";
-	private String name = "root";
-	private String password = "root";// 123456
-	private Connection con = null;//暂不考虑静态
-	private Statement stmt = null;
-	private ResultSet res = null;
+	private static final String driver = "com.mysql.jdbc.Driver"; // 驱动
+	private static final String url = "jdbc:mysql://localhost:3306/kjb_sign";
+	private static final String name = "root";
+	private static final String password = "root";// 123456
+	private static Connection con = null;
 
-	/** 构造器：建立数据连接 */
-	public DbMain() {
-		try {// 建立连接数据库的连接
-			Class.forName(driver); // 加载驱动程序
-			con = DriverManager.getConnection(url, name, password); // 连接数据库
-			stmt = con.createStatement();
-		} catch (Exception e) {
-			System.out.println("--数据库连接错误--");
-		}
+	/** 获取数据库连接 */
+	public static Connection getConnect() {
+		if (con == null)
+			try {// 建立连接数据库的连接
+				Class.forName(driver); // 加载驱动程序
+				con = DriverManager.getConnection(url, name, password); // 连接数据库
+			} catch (Exception e) {
+				System.out.println("--数据库连接错误--");
+			}
+		return con;
 	}
 
-	
 }
--- src/database/DbUser.java
+++ src/database/DbUser.java
@@ -1,5 +1,105 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class DbUser {
+	private Connection con = null;
+
+	public DbUser() {
+		con = DbMain.getConnect();
+	}
+
+	public String stuLogin(String name, String pass, boolean need_class) {
+		Statement stmt = null;
+		ResultSet res = null;
+		String sql = "";
+		String result = "";
+		try {
+			stmt = con.createStatement();
+			sql = "select * from user_t where name='" + name + "' and pass='"
+					+ pass + "'";
+			res = stmt.executeQuery(sql);
+			if (res.next()) { // 登录成功
+				if (need_class) {
+					// ////////////////考虑是否返回课程表///////////////注意stmt是否冲突
+					result = "1";
+				} else {
+					result = "1";
+				}
+			} else {
+				/////////////////////未注册处理//////////////////////
+				result = "-1";
+			}
+			stmt.close();
+			res.close();
+		} catch (Exception ex) {
+			System.out.println("登录异常");
+			result = "-2";
+		}
+		return result;
+	}
+
+	public String teaLogin(String name, String pass, boolean need_class) {
+		Statement stmt = null;
+		ResultSet res = null;
+		String sql = "";
+		String result = "";
+		try {
+			stmt = con.createStatement();
+			sql = "select * from user_t where name='" + name + "' and pass='"
+					+ pass + "'";
+			res = stmt.executeQuery(sql);
+			if (res.next()) { // 登录成功
+				if (need_class) {
+					// ////////////////考虑是否返回课程表///////////////注意stmt是否冲突
+					result = "1";
+				} else {
+					result = "1";
+				}
+			} else {
+				result = "-1";
+			}
+			stmt.close();
+			res.close();
+		} catch (Exception ex) {
+			System.out.println("登录异常");
+			result = "-2";
+		}
+		return result;
+	}
+
+	public String teaRegister(String name, String pass, String real) {
+		Statement stmt = null;
+		ResultSet res = null;
+		String sql = "";
+		try {// 是否账号重复
+			stmt = con.createStatement();
+			sql = "select * from user_t where name='" + name + "' or real='"
+					+ real + "'";
+			res = stmt.executeQuery(sql);
+			if (res.next()) {
+				stmt.close();
+				res.close();
+				return "-1";
+			}
+		} catch (Exception ex) {// 都报错了，就不必close了
+			return "-2";
+		}
+		try {// 注册
+			sql = "insert into user_t values('" + name + "','" + pass + "','"
+					+ real + "')";
+			int num = stmt.executeUpdate(sql);
+			stmt.close();
+			res.close();
+			if (num != 0) // 修改成功
+				return "1";
+			return "-2";
+		} catch (Exception e) {
+			System.out.println("注册异常");
+			return "-2";
+		}
+	}
 
 }
